9aa540a560e7eaf7975e6d30a7397588abc144f2
ALmsq <alexander.mosquea3@gmail.com>
2020-06-18 15:58:26 -0400

    semi-working search

--- src/App.js
+++ src/App.js
@@ -1,48 +1,92 @@
-import React, { useEffect, useState, useRef } from 'react'
-import axios from 'axios'
-import _ from 'lodash'
-
-const Auto = () => {
-  const [display, setDisplay] = useState(false) //show search
-  const [options, setOptions] = useState([])
-  const [search, setSearch] = useState('')
-
-  useEffect(() => {
-    let stocks = []
-    let term = 'btc'
-    setSearch(term)
-    axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=QZ4L2TSNZBWXXVTO`)
-    .then(res => {
-      console.log(res.data);
-      let data = res.data
-      console.log(data.bestMatches);
-      // let stock = data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )
-      let stock = _.flattenDeep(Array.from(data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )))
-
-      stocks.push(stock)
-
-    })
-
-    setOptions(stocks)
-    console.log(options);
-    console.log(stocks);
-    console.log(term);
-    console.log(search);
-  },[])
-
-  return(
-    <div>yo</div>
-  )
-}
-
-function App() {
-  return(
-    <div>
-      <Auto/>
-    </div>
-  )
-}
-export default App
+// import React, { useEffect, useState, useRef } from 'react'
+// import axios from 'axios'
+// import _ from 'lodash'
+//
+// const Auto = () => {
+//   const [display, setDisplay] = useState(false) //show search
+//   const [options, setOptions] = useState([])
+//   const [search, setSearch] = useState('btc')
+//
+//   let stocks = []
+//   useEffect(() => {
+//
+//     let term = 'btc'
+//     setSearch(term)
+//     axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=QZ4L2TSNZBWXXVTO`)
+//     .then(res => {
+//       console.log(res.data);
+//       let data = res.data
+//       console.log(data.bestMatches);
+//       // let stock = data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )
+//       let stock = _.flattenDeep(Array.from(data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )))
+//       console.log(stock);
+//       stocks.push(stock)
+//
+//     })
+//     setOptions(...options, stocks)
+//
+//     // console.log('options: ',options);
+//     // console.log('stocks: ',stocks);
+//     // console.log(term);
+//     // console.log('search: ',search);
+//   },[])
+//
+//
+//   // console.log('options: ',options);
+//
+//
+//
+//
+//   const updateSearch = e =>{
+//     // setSearch((e.target.value))
+//     console.log(e);
+//     console.log(e);
+//     console.log(options);
+//     console.log(search);
+//   }
+//
+//
+//   return(
+//     <div>
+//       <input
+//         id='auto'
+//         onClick={()=> setDisplay(!display)}
+//         placeholder='type to search'
+//         value={search}
+//         onChange={event => setSearch(event.target.value)}
+//       />
+//       // {console.log(display)}
+//       {display && (
+//         <div>
+//           {options.map((value, i) => {
+//             return(
+//               <div
+//                 onClick={() => updateSearch(value)}
+//                 key={i}
+//                 tabIndex='0'
+//               >
+//               {console.log(value)}
+//               <span>{value[0]['symbol']}</span>
+//               // {console.log(value)}
+//               </div>
+//             )
+//           })
+//
+//           }
+//         </div>
+//       )}
+//     </div>
+//   )
+// }
+//
+// function App() {
+//   return(
+//     <div>
+//       <Auto/>
+//     </div>
+//   )
+// }
+// export default App
 
 ///////////////////////////////////////
 
@@ -73,8 +117,9 @@ export default App
 //     });
 //
 //     console.log(pokemon);
-//     console.log(options);
+//     console.log('options', options);
 //     setOptions(pokemon);
+//     console.log(options);
 //   }, []);
 //
 //   useEffect(() => {
@@ -93,6 +138,8 @@ export default App
 //
 //   const updatePokeDex = poke => {
 //     setSearch(poke);
+//     console.log(search);
+//     console.log(options);
 //     setDisplay(false);
 //   };
 //
@@ -104,6 +151,8 @@ export default App
 //         placeholder="Type to search"
 //         value={search}
 //         onChange={event => setSearch(event.target.value)}
+//
+//
 //       />
 //       {display && (
 //         <div className="autoContainer">
@@ -141,3 +190,56 @@ export default App
 // }
 //
 // export default App;
+////////////////////////////////
+
+import React, { useEffect, useState, useRef } from 'react'
+import axios from 'axios'
+import _ from 'lodash'
+
+const Auto = () => {
+  const [display, setDisplay] = useState(false) //show search
+  const [options, setOptions] = useState([])
+  const [search, setSearch] = useState('btc')
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    let stocks = []
+    fetch(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=QZ4L2TSNZBWXXVTO`)
+      .then(res => res.json())
+      .then((data) => {
+        console.log(data);
+        stocks = [...stocks, data ]
+        setOptions(stocks)
+      })
+  }, [search])
+
+
+let handleChange = (e) => {
+  console.log(e);
+}
+
+  return(
+    <div>
+      <input
+        placeholder='type to search'
+        onChange={(e) => setSearch(e.target.value)}
+      />
+      {console.log('search: ',search)}
+      {console.log('options: ',options)}
+    </div>
+  )
+}
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Custom AutoComplete React</h1>
+      <div className="logo"></div>
+      <div className="auto-container">
+        <Auto />
+      </div>
+    </div>
+  );
+}
+
+export default App;

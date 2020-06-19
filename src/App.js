b97fbd075557e92f81cb2a602670f2e302a6e394
ALmsq <alexander.mosquea3@gmail.com>
2020-06-19 02:49:32 -0400

    search results saved to clickedStock state

--- src/App.js
+++ src/App.js
@@ -1,196 +1,3 @@
-// import React, { useEffect, useState, useRef } from 'react'
-// import axios from 'axios'
-// import _ from 'lodash'
-//
-// const Auto = () => {
-//   const [display, setDisplay] = useState(false) //show search
-//   const [options, setOptions] = useState([])
-//   const [search, setSearch] = useState('btc')
-//
-//   let stocks = []
-//   useEffect(() => {
-//
-//     let term = 'btc'
-//     setSearch(term)
-//     axios.get(`https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${search}&apikey=QZ4L2TSNZBWXXVTO`)
-//     .then(res => {
-//       console.log(res.data);
-//       let data = res.data
-//       console.log(data.bestMatches);
-//       // let stock = data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )
-//       let stock = _.flattenDeep(Array.from(data.bestMatches.map((stock) =>[{symbol: stock['1. symbol'], name: stock['2. name'] }] )))
-//       console.log(stock);
-//       stocks.push(stock)
-//
-//     })
-//     setOptions(...options, stocks)
-//
-//     // console.log('options: ',options);
-//     // console.log('stocks: ',stocks);
-//     // console.log(term);
-//     // console.log('search: ',search);
-//   },[])
-//
-//
-//   // console.log('options: ',options);
-//
-//
-//
-//
-//   const updateSearch = e =>{
-//     // setSearch((e.target.value))
-//     console.log(e);
-//     console.log(e);
-//     console.log(options);
-//     console.log(search);
-//   }
-//
-//
-//   return(
-//     <div>
-//       <input
-//         id='auto'
-//         onClick={()=> setDisplay(!display)}
-//         placeholder='type to search'
-//         value={search}
-//         onChange={event => setSearch(event.target.value)}
-//       />
-//       // {console.log(display)}
-//       {display && (
-//         <div>
-//           {options.map((value, i) => {
-//             return(
-//               <div
-//                 onClick={() => updateSearch(value)}
-//                 key={i}
-//                 tabIndex='0'
-//               >
-//               {console.log(value)}
-//               <span>{value[0]['symbol']}</span>
-//               // {console.log(value)}
-//               </div>
-//             )
-//           })
-//
-//           }
-//         </div>
-//       )}
-//     </div>
-//   )
-// }
-//
-// function App() {
-//   return(
-//     <div>
-//       <Auto/>
-//     </div>
-//   )
-// }
-// export default App
-
-///////////////////////////////////////
-
-// import React, { useEffect, useState, useRef } from "react";
-// import logo from "./logo.svg";
-// import "./App.css";
-//
-// const Auto = () => {
-//   const [display, setDisplay] = useState(false);
-//   const [options, setOptions] = useState([]);
-//   const [search, setSearch] = useState("");
-//   const wrapperRef = useRef(null);
-//
-//   useEffect(() => {
-//     const pokemon = [];
-//     const promises = new Array(20)
-//       .fill()
-//       .map((v, i) => fetch(`https://pokeapi.co/api/v2/pokemon-form/${i + 1}`));
-//     Promise.all(promises).then(pokemonArr => {
-//       return pokemonArr.map(value =>
-//         value
-//           .json()
-//           .then(({ name, sprites: { front_default: sprite } }) =>
-//             pokemon.push({ name, sprite })
-//
-//           )
-//       );
-//     });
-//
-//     console.log(pokemon);
-//     console.log('options', options);
-//     setOptions(pokemon);
-//     console.log(options);
-//   }, []);
-//
-//   useEffect(() => {
-//     window.addEventListener("mousedown", handleClickOutside);
-//     return () => {
-//       window.removeEventListener("mousedown", handleClickOutside);
-//     };
-//   });
-//
-//   const handleClickOutside = event => {
-//     const { current: wrap } = wrapperRef;
-//     if (wrap && !wrap.contains(event.target)) {
-//       setDisplay(false);
-//     }
-//   };
-//
-//   const updatePokeDex = poke => {
-//     setSearch(poke);
-//     console.log(search);
-//     console.log(options);
-//     setDisplay(false);
-//   };
-//
-//   return (
-//     <div ref={wrapperRef} className="flex-container flex-column pos-rel">
-//       <input
-//         id="auto"
-//         onClick={() => setDisplay(!display)}
-//         placeholder="Type to search"
-//         value={search}
-//         onChange={event => setSearch(event.target.value)}
-//
-//
-//       />
-//       {display && (
-//         <div className="autoContainer">
-//           {options
-//             .filter(({ name }) => name.indexOf(search.toLowerCase()) > -1)
-//             .map((value, i) => {
-//               return (
-//                 <div
-//                   onClick={() => updatePokeDex(value.name)}
-//                   className="option"
-//                   key={i}
-//                   tabIndex="0"
-//                 >
-//                   <span>{value.name}</span>
-//                   <img src={value.sprite} alt="pokemon" />
-//                 </div>
-//               );
-//             })}
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-//
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>Custom AutoComplete React</h1>
-//       <div className="logo"></div>
-//       <div className="auto-container">
-//         <Auto />
-//       </div>
-//     </div>
-//   );
-// }
-//
-// export default App;
-////////////////////////////////
 
 import React, { useEffect, useState, useRef } from 'react'
 import axios from 'axios'
@@ -203,12 +10,12 @@ const Auto = () => {
   const [search, setSearch] = useState('btc')
   const [data, setData] = useState()
   const timeoutRef = useRef(null)
-  const [value, setValue] = useState('')
+  // const [value, setValue] = useState('')
+  const [clickedStock, setClickedStock] = useState('')
 
   function validate() {
     console.log('validating after 5ms..');
   }
-
 
   useEffect(() => {
     let stocks = []
@@ -258,20 +65,25 @@ let returnResults = (stock) => {
       <input
         placeholder='type to search'
         onChange={(e) => setSearch(e.target.value || 'btc')}
-        // value = {search}
+        // value = {value}
       />
+
       {console.log('search: ',search)}
       {console.log('options: ',options)}
       {options.map((stock, i) => {
         // console.log('test', stock, i);
+          let symbol = stock['1. symbol']
+          let name = stock['2. name']
         return(
-          <div onClick ={(e) => console.log(i)}
+          <div onClick ={(e) => setClickedStock(symbol)}
           key = {i}
           >
-            <span>{stock['1. symbol']} - {stock['2. name']}</span>
+          {console.log('click',clickedStock)}
+            <span>{symbol} - {name}</span>
           </div>
         )
       })}
+
     </div>
   )
 }

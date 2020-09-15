d02c363fa74f41db173b9b7ab213def2a22ae50d
ALmsq <alexander.mosquea3@gmail.com>
2020-09-15 17:34:22 -0400

    added user page to menu

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React from 'react'
+import { useSelector } from 'react-redux'
 import { StyledMenu } from './Menu.styled';
 
 const Menu = ({ open }) => {
+
+  const user = useSelector(state => state.user.credentials)
   return (
     <StyledMenu open={open}>
+      {user.username ? <a href="/userpage">
+        <span role="img" aria-label="about us">💻</span>
+        {user.username}'s Page
+      </a> : null}
       <a href="/">
         <span role="img" aria-label="about us">🏠</span>
         Home
